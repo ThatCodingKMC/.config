410f25331b6f0d800bbdddbab8457bf414051e0a
Kyle Cahalan <kylemcahalan@gmail.com>
2025-02-03 00:23:54 -0500

    Added package manager detection to installer script

--- RUNME.sh
+++ RUNME.sh
@@ -8,15 +8,36 @@
 # git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 # Needed maybe to get other plugins, I should script the getting of plugins and to installing of them.
 # Check for git ahead of running script, either get git or close script.
-
+# get gdb, gcc, python, pwn tools, etc.
 # Add nvim mkviews for the files to the install so it can come prefolded.
+
+
+# Find and store the package manager, exit if not found.
+PACKAGE_MANAGER="unknown"
+
+command -v apt-get  && PACKAGE_MANAGER="apt-get"
+command -v yum      && PACKAGE_MANAGER="yum"
+command -v dnf      && PACKAGE_MANAGER="dnf"
+command -v zypper   && PACKAGE_MANAGER="zypper"
+command -v pacman   && PACKAGE_MANAGER="pacman"
+
+if ["$PACKAGE_MANAGER" == "unknown"]; then 
+  echo "\e[31mERROR: supported package manager not found (apt, yum, dnf, zypper, pacman).\e[0m"
+  exit 1
+fi
+
+echo "Detected package manager: $PACKAGE_MANAGER"
+
+
+
+
 
 
 # MUST BE DONE AFTER TMUX IS SET UP
 # Install Tmux plugin manager.
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+### git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 # Run the plugin installer to get the plugins specified in the configs. 
-~/.config/tmux/plugins/tpm/bin/install_plugins
+### ~/.config/tmux/plugins/tpm/bin/install_plugins
 
 
 
